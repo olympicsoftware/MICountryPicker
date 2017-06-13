e28b7544fa59e8e7b2b770d1f8c0f37f571c74f8
Mike Little <michaell@olympic.co.nz>
2017-06-13 15:45:25 +1200

    Create framework and rename demo app

---
+++ CountryPicker/CountryPicker.h
@@ -0,0 +1,19 @@
+//
+//  CountryPicker.h
+//  CountryPicker
+//
+//  Created by Mike Little on 13/06/17.
+//  Copyright © 2017 Mustafa Ibrahim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CountryPicker.
+FOUNDATION_EXPORT double CountryPickerVersionNumber;
+
+//! Project version string for CountryPicker.
+FOUNDATION_EXPORT const unsigned char CountryPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountryPicker/PublicHeader.h>
+
+
